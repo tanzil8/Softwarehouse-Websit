f376caac37222cb3ea144abf52b39185bdbf2855
tanzil8 <khantanzil383@gmail.com>
2025-05-12 13:26:47 -0700

    logo.jsx comp

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,12 +1,13 @@
+import Image from "next/image";
 import Link from "next/link"
 
 const Logo = ({light = false}) => {
     //determined the text color based on the light prop
     const colorClass = light ? "text-white" : "text-primary" // default to black
   return (
-  <Link href="/" className="font-primary text-4xl tracking-[4px]">
- <img src="/assets/contact/img28.png" alt="" width={300} />
-  </Link>
+ <Link href="/" className="hidden lg:block font-primary text-4xl tracking-[4px]">
+      <Image src="/assets/contact/img28.png" alt="Logo" width={300} height={100} priority />
+    </Link>
   );
 }
 
